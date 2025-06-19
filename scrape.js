f7ad786e33cb678e182a60b19b85b85afc0b4a68
Abhinav Shinde <143414991+Abhi9shinde@users.noreply.github.com>
2025-06-19 16:47:42 +0530

    workflow_error1

--- scrape.js
+++ scrape.js
@@ -134,6 +134,7 @@ async function scrapeCompany(page, name, url) {
 (async () => {
   const browser = await puppeteer.launch({
     headless: true,
+    args: ["--no-sandbox", "--disable-setuid-sandbox"],
   });
 
   const page = await browser.newPage();
